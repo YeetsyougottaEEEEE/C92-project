b192f1b0d03cd8649061d37dec188c1a44890cee
YeetsyougottaEEEEE <67150450+YeetsyougottaEEEEE@users.noreply.github.com>
2021-05-15 14:40:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import WelcomeScreen from './screen/WelcomeScreen';
+import { createAppContainer, createSwitchNavigator} from 'react-navigation';
+import MainScreen from './screen/MainScreen';
+
+
+export default class App extends React.Component {
+  render(){
+  return (
+    <View   style={styles.container}>
+     <AppContainer/>
+    </View>
+  );
+}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen : WelcomeScreen,
+  MainScreen : MainScreen
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
+
